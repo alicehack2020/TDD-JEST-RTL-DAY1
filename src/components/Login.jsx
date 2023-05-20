4daf31c834c6f829f65618bda9c722c2f7f6945f
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2023-05-20 19:26:39 +0530

    onchange and please wait...

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,46 @@
+// import axios from 'axios'
 import React, { useState } from 'react'
 
 const Login = () => {
     const [username,setUserName]=useState("")
-    const [password,setPassword]=useState("")
+    const [password, setPassword] = useState("")
+    const [error,setError]=useState(false)
+    const [loading,setLoading]=useState(false)
+    const [user,setUser]=useState()
+  
+  
+  // const handdleClick = async (e) => {
+  //   e.preventDefault()
+  //   try {
+  //     const {data}=await axios.get("https://jsonplaceholder.typicode.com/todos/1")
+  //     setUser(data)
+  //   } catch (error) {
+      
+  //   }
+    
+  // }
+  const handdleClick = (e) => {
+    e.preventDefault()
+    setLoading(true)
+    try {
+      setTimeout(() => {
+        setLoading(false) 
+      },5000)
+    } catch (error) {
+      console.log(error)
+      setError(true)
+      setLoading(false) 
+    }
+     
+  }
+  
   return (
       <div>
           <form action="">
-              <input type="text" placeholder='enter name' value={username} />
-              <input type="password" placeholder='enter password' value={password} />
-              <button>login</button>
+        <input type="text" placeholder='enter name' value={username} onChange={e => setUserName(e.target.value)} />
+        <input type="password" placeholder='enter password' value={password} onChange={e => setPassword(e.target.value)} />
+        <button disabled={!username || !password} onClick={handdleClick}>{loading?"please wait":"login"}</button>
+        <span data-testid="error" style={{visibility:error?"visible":"hidden"}}>some thing went wrong</span>
           </form>
     </div>
   )
--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import Login from "./Login"
 
 // render
@@ -34,4 +34,67 @@ test("password input should be empty", () => {
     expect(userInputEl.value).toBe("")
 })
 
+//button initially disabled
+test("login button should disable initially", () => {
+    render(<Login />)
+    const buttonInputEl = screen.getByRole("button")
+    expect(buttonInputEl).toBeDisabled()
+})
+
+test("loading should not be render", () => {
+    render(<Login />)
+    const buttonInputEl = screen.getByRole("button")
+    expect(buttonInputEl).not.toHaveTextContent(/please wait/i)
+})
+
+//initially error mesage should hidden
+test("login error should invisiable initially", () => {
+    render(<Login />)
+    const errorEl = screen.getByTestId("error")
+    expect(errorEl).not.toBeVisible()
+})
+
+//value should change using on change
+test("username input should change", () => {
+    render(<Login />)
+    const userInputEl = screen.getByPlaceholderText(/enter name/i)
+    const testuser = "mangesh"
+    fireEvent.change(userInputEl,{target:{value:testuser}})
+    expect(userInputEl.value).toBe(testuser)
+})
+
+test("password input should be change", () => {
+    render(<Login />)
+    const passwordInputEl = screen.getByPlaceholderText(/enter password/i)
+    const testpassword = "pass@1234"
+    fireEvent.change(passwordInputEl, { target: { value:testpassword } })
+    expect(passwordInputEl.value).toBe(testpassword)
+})
+
+test("button should not be disable when input exist", () => {
+    render(<Login />)
+    const buttonEl=screen.getByRole("button")
+    const userInputEl=screen.getByPlaceholderText(/enter name/i)
+    const passwordInputEl = screen.getByPlaceholderText(/enter password/i)
+    const testuser="mangesh"
+    const testpassword = "pass@123"
+    fireEvent.change(userInputEl, { target: { value: testuser } })
+    fireEvent.change(passwordInputEl, { target: { value: testpassword } })
+    expect(buttonEl).not.toBeDisabled()
+})
+
+test("loading should  be rendered when click ", () => {
+    render(<Login />)
+    const buttonInputEl = screen.getByRole("button")
+    const userInputEl=screen.getByPlaceholderText(/enter name/i)
+    const passwordInputEl = screen.getByPlaceholderText(/enter password/i)
+    const testuser="mangesh"
+    const testpassword = "pass@123"
+    
+    fireEvent.change(userInputEl, { target: { value: testuser } })
+    fireEvent.change(passwordInputEl, { target: { value: testpassword } })
+    fireEvent.click(buttonInputEl)
+
+    expect(buttonInputEl).toHaveTextContent(/please wait/i)
+})
 

69d01ecd668a4b52fef0d927b5300374d4de83bc
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2023-05-20 17:26:29 +0530

    input should empty initially

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [username,setUserName]=useState("")
+    const [password,setPassword]=useState("")
   return (
       <div>
           <form action="">
-              <input type="text" placeholder='enter name' />
-              <input type="password" placeholder='enter password' />
+              <input type="text" placeholder='enter name' value={username} />
+              <input type="password" placeholder='enter password' value={password} />
               <button>login</button>
           </form>
     </div>
--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react"
 import Login from "./Login"
 
+// render
 test("username input should be render", () => {
     render(<Login />)
     const userInputEl = screen.getByPlaceholderText(/enter name/i)
@@ -13,9 +14,24 @@ test("userpassword input should be render", () => {
     expect(passwordInputEl).toBeInTheDocument()
 })
 
-test("userpassword input should be render", () => {
+test("button should be render", () => {
     render(<Login />)
     const buttonInputEl = screen.getByRole("button")
     expect(buttonInputEl).toBeInTheDocument()
 })
+
+//input should be empty initially
+
+test("usernmae input should be empty", () => {
+    render(<Login />)
+    const userInputEl = screen.getByPlaceholderText(/enter name/i)
+    expect(userInputEl.value).toBe("")
+})
+
+test("password input should be empty", () => {
+    render(<Login />)
+    const userInputEl = screen.getByPlaceholderText(/enter password/i)
+    expect(userInputEl.value).toBe("")
+})
+
 
